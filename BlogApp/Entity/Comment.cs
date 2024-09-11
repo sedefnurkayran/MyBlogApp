5c7e7f486351d36c308eb2f507e5d00ec29d1d40
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2024-09-12 00:09:25 +0300

    Entities are created

---
+++ BlogApp/Entity/Comment.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Entity
+{
+
+    public class Comment
+    {
+
+        [Key]
+        public int CommentId { get; set; }
+        public string? Text { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!; //User hakkindaki her seyi cekebiliriz. 
+    }
+}
