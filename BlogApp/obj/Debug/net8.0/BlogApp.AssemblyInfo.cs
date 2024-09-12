2fee137e018927dc630345f99f32a3d3721ecfb7
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2024-09-12 17:01:40 +0300

    Project has been renamed

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c7e7f486351d36c308eb2f507e5d00ec29d1d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d721ed9ae29ce77d4b41caf1d7738d2813423d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
